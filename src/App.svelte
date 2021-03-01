88ff016c73ec5352d7c8b05bd1ac51e19a615f8a
Romain <romain.sire@gmx.com>
2021-03-01 22:52:27 +0100

    app startup

--- src/App.svelte
+++ src/App.svelte
@@ -1,54 +1,22 @@
 <script lang="ts">
   import Router from "svelte-spa-router";
   import routes from "./routes";
-  export let name: string;
+  import Header from "./components/header.svelte";
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
-  <hr />
-  <h1>Test SCSS</h1>
-  <div>
-    <p>ça devrait être bleu ! :)</p>
-  </div>
-  <hr />
-  <h1>Test Navigation</h1>
-  <nav>
-    <ul>
-      <li><a href="#/">Home</a></li>
-      <li><a href="#/about">About</a></li>
-    </ul>
-  </nav>
+  <Header />
   <Router {routes} />
 </main>
 
 <style lang="scss">
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-  div {
-    p {
-      color: blue;
-    }
+    background-image: url("../img/murLille.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: right 5% bottom 20%;
+    width: 100vw;
+    min-height: 100vh;
   }
 </style>
---
+++ src/components/header.svelte
@@ -0,0 +1,15 @@
+<header>
+  <h1>header</h1>
+  <nav>
+    <ul>
+      <li><a href="#/">Home</a></li>
+      <li><a href="#/about">About</a></li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  header {
+    background: rgba(206, 69, 69, 0.623);
+  }
+</style>
