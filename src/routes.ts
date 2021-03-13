47a71eba1bd06f25aed15c26187342bbfddbcfba
Romain <romain.sire@gmx.com>
2021-03-14 00:29:17 +0100

    Remove test 'about' page

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,7 @@
 import Home from './pages/home.svelte';
-import About from './pages/about.svelte';
 import FourOhFour from './pages/fourohfour.svelte';
 
 export default {
     '/': Home,
-    '/about': About,
     '*': FourOhFour,
 }
