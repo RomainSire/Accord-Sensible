b5a4dac78860a7403755a6ccff5022efc2954e51
Romain <romain.sire@gmx.com>
2021-04-02 21:41:15 +0200

    add 'reparation' page

---
+++ src/pages/reparations.svelte
@@ -0,0 +1,44 @@
+<div class="wrapper">
+  <div class="hero" />
+  <div class="description">
+    <h1>Réparations</h1>
+
+    <p>
+      Les réparations que je suis en mesure de vous proposer sont les suivantes
+      :
+    </p>
+    <ol>
+      <li><strong>Changement de cordes cassées</strong></li>
+      <li>
+        <strong>Remplacement des axes de fourches</strong> (de marteau / de chevalet
+        / de bâton d'échappement / ou d'étouffoir), on parle aussi de repivotage
+      </li>
+      <li>
+        <strong>Recollage ou remplacement de pièces</strong> (revêtements de touche
+        en ivoire, manches de marteaux, fourches, ...)
+      </li>
+      <li>
+        <strong>Regarnissage</strong> (il s'agit de recoller les feutres des mortaises,
+        des trous d'axe, des pédales, ...)
+      </li>
+      <li>
+        <strong>Dépannage</strong> (rebonds sur les cordes, problèmes de répétition
+        ou d'absence de son, blocages de touches , ...)
+      </li>
+    </ol>
+    <p>
+      Le devis est gratuit lorsque les réparations sont importantes, sinon le
+      travail est fait "dans la foulée" à l'occasion d'un accord par exemple.
+    </p>
+  </div>
+</div>
+
+<style lang="scss">
+  @import "../commons.scss";
+
+  @include hero-description-layout;
+
+  .hero {
+    background-image: url("../../img/rechevillage.jpg");
+  }
+</style>
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import Home from './pages/home.svelte';
 import Accord from "./pages/accord.svelte";
 import Reglages from "./pages/reglages.svelte";
+import Reparations from "./pages/reparations.svelte";
 import FourOhFour from './pages/fourohfour.svelte';
 
 export default {
     '/': Home,
     '/accord': Accord,
     '/reglages': Reglages,
+    '/reparations': Reparations,
     '*': FourOhFour,
 }
