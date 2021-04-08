1b208b7c7bb39108b91005dfdde1bb4974605d4c
Romain <romain.sire@gmx.com>
2021-04-08 22:15:18 +0200

    add 404 page

--- src/pages/fourohfour.svelte
+++ src/pages/fourohfour.svelte
@@ -1,1 +1,18 @@
-<h2>404!! :(</h2>
+<div class="wrapper">
+  <div class="hero" />
+  <div class="description">
+    <h1>404</h1>
+    <p>La page demandée n'existe pas&nbsp!</p>
+    <p>Vous pouvez utiliser le menu pour consulter une autre page...</p>
+  </div>
+</div>
+
+<style lang="scss">
+  @import "../commons.scss";
+
+  @include main-layout;
+
+  .hero {
+    background-image: url("../../img/404.jpg");
+  }
+</style>
